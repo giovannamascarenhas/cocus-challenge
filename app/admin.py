aff890a835d9e22750610efb2a8780dd3b1de9b5
giovannamascarenhas <giovanna.mascarenhas.1@gmail.com>
2020-09-16 10:44:35 +0100

    Creating the text model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TextModel
 
-# Register your models here.
+
+admin.site.register(TextModel)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class TextModel(models.Model):
+    text_line = models.CharField(max_length=1000)
+    most_frequency_character = models.CharField(max_length=100)
+    line_index = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.text_line
