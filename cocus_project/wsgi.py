45b276652769be1f1f2025e5a2cb70a96a28b139
giovannamascarenhas <giovanna.mascarenhas.1@gmail.com>
2020-09-17 15:50:55 +0100

    Adding configuration for static files

--- cocus_project/wsgi.py
+++ cocus_project/wsgi.py
@@ -3,14 +3,13 @@ WSGI config for cocus_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cocus_project.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
